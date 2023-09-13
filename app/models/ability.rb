8888d46e4cb2c4c58b487cf81207236e6a19d3b9
Musangamfura Emmanuel <musangamfure@gmail.com>
2023-09-13 12:56:50 +0100

    Generate and update ability model

---
+++ app/models/ability.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    can :destroy, Group do |group|
+      group.user == user
+    end
+
+    can :update, Group do |group|
+      group.user == user
+    end
+
+    can :destroy, Bill do |bill|
+      bill.author == user
+    end
+
+    can :update, Bill do |bill|
+      bill.author == user
+    end
+
+    can :create, Group
+    can :create, Bill
+  end
+end
